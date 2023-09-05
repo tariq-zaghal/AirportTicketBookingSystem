a9709796fa526646f98a78ddc23bffec980bbfe3
Tariq Zaghal <zaghaltariq007@gmail.com>
2023-09-05 15:11:46 +0300

    FEAT: Create Enum to map options passenger can choose from to certain integers

---
+++ PassengerHandler/PassengerOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTicketBookingSystem.PassengerHandler
+{
+    public enum PassengerOptions
+    {
+        BookFlight = 1,
+        EditABooking = 2,
+        ExitPassenger = 3
+    }
+}
