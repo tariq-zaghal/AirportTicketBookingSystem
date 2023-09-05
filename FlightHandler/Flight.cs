87955a5939bb9b1c4f87bbfd17814840289f486a
Tariq Zaghal <zaghaltariq007@gmail.com>
2023-09-05 13:33:28 +0300

    FEAT: Add validation attributes to fields of Flight.cs

--- FlightHandler/Flight.cs
+++ FlightHandler/Flight.cs
@@ -1,25 +1,46 @@
-﻿using System.Linq;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using AirportTicketBookingSystem.BookingHandler;
+using AirportTicketBookingSystem.Validation;
 
 namespace AirportTicketBookingSystem.FlightHandler
 {
     public class Flight
     {
         private static int idGenerator = 0;
+
+        [Required(ErrorMessage = "Departure Country: Required")]
+        [MinLength(2, ErrorMessage = "Departure Country: Minimum length is 2 characters")]
+        [MaxLength(25, ErrorMessage = "Departure Country: Maximum length is 25 characters")]
         public string DepartureCountry { get; init; }
 
+        [Required(ErrorMessage = "Departure Time: Required")]
+        [IsDateValid(ErrorMessage = "Departure Time: Depature time should be later than current time")]
         public DateTime DepartureDateTime { get; init; }
 
+        [Required(ErrorMessage = "Departure Airport: Required")]
+        [MinLength(2, ErrorMessage = "Departure Airport: Minimum length is 2 characters")]
+        [MaxLength(20, ErrorMessage = "Departure Airport: Maximum length is 20 characters")]
         public string DepartureAirport { get; init; }
 
+        [Required(ErrorMessage = "Destination Country: Required")]
+        [MinLength(2, ErrorMessage = "Destination Country: Minimum length is 2 characters")]
+        [MaxLength(25, ErrorMessage = "Destination Country: Maximum length is 25 characters")]
         public string DestinationCountry { get; init; }
 
+        [Required(ErrorMessage = "Destination Airport: Required")]
+        [MinLength(2, ErrorMessage = "Destination Airport: Minimum length is 2 characters")]
+        [MaxLength(20, ErrorMessage = "Destination Airport: Maximum length is 20 characters")]
         public string DestinationAirport { get; init; }
 
+        [Required(ErrorMessage = "Base Price: Required")]
+        [Range(50,10000, ErrorMessage = "Base Price: Should we between 50 and 10000")]
         public decimal FlightBasePrice { get; init; }
         
         public int FlightId { get; init; }
 
+        [Required(ErrorMessage = "Number of Seats: Required")]
+        [Range(6, 500, ErrorMessage = "Number of Seats: Should be between 6 and 500")]
         public int NumberOfSeats { get; init; } = 150;
 
         private List<int> _flightSeats;
