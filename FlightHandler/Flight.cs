75b3a026dffc5c05a29e179850397665136767b6
Tariq Zaghal <zaghaltariq007@gmail.com>
2023-09-05 12:52:27 +0300

    FEAT: Add booking-related methods to Flight.cs (Add,Remove,Get) and Add new fields

--- FlightHandler/Flight.cs
+++ FlightHandler/Flight.cs
@@ -5,9 +5,10 @@ namespace AirportTicketBookingSystem.FlightHandler
 {
     public class Flight
     {
+        private static int idGenerator = 0;
         public string DepartureCountry { get; init; }
 
-        public DateTime DepartureTime { get; init; }
+        public DateTime DepartureDateTime { get; init; }
 
         public string DepartureAirport { get; init; }
 
@@ -16,8 +17,10 @@ namespace AirportTicketBookingSystem.FlightHandler
         public string DestinationAirport { get; init; }
 
         public decimal FlightBasePrice { get; init; }
+        
+        public int FlightId { get; init; }
 
-        public int SizeOfPlane { get; init; } = 150;
+        public int NumberOfSeats { get; init; } = 150;
 
         private List<int> _flightSeats;
 
@@ -26,23 +29,41 @@ namespace AirportTicketBookingSystem.FlightHandler
         public Flight()
         {
             PopulateSeatsOfPlane();
+            _bookings = new List<Booking>();
+            FlightId = ++idGenerator;
         }
 
         private void PopulateSeatsOfPlane()
         {
-            _flightSeats = Enumerable.Range(1, SizeOfPlane).ToList();
+            _flightSeats = Enumerable.Range(1, NumberOfSeats).ToList();
         }
 
         public List<int> getSeats()
         {
             return _flightSeats;
         }
+        
+        public void AddBooking(Booking booking)
+        {
+            _bookings.Add(booking);
+        }
+
+        public void RemoveBooking(Booking booking)
+        {
+            _bookings.Remove(booking);
+        }
+
+        public List<Booking> GetBookings()
+        {
+            return _bookings;
+        }
+
 
         public override string ToString()
         {
             return $"Departure country = {DepartureCountry}\n" +
                    $"Departure Airport = {DepartureAirport}\n" +
-                   $"Departure Time = {DepartureTime}\n" +
+                   $"Departure Time = {DepartureDateTime}\n" +
                    $"Destination country = {DestinationCountry}\n" +
                    $"Destination Airport = {DestinationAirport}\n" +
                    $"Base price = {FlightBasePrice}\n";
