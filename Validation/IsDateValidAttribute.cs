66649aaebdb0501391c8d226749ad12f922514c4
Tariq Zaghal <zaghaltariq007@gmail.com>
2023-09-05 13:39:32 +0300

    FEAT: Create Custome feature to check the validity of Departurm Time in Flight.cs

---
+++ Validation/IsDateValidAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTicketBookingSystem.Validation
+{
+    public class IsDateValidAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            if (value is DateTime dateTime)
+            {
+                return dateTime >= DateTime.Now;
+            }
+
+            return false;
+        }
+    }
+}
