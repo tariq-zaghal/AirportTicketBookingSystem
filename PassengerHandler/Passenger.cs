2b4ad54b356f0c20219d46e772fdefa2f0d790a6
Tariq Zaghal <zaghaltariq007@gmail.com>
2023-09-05 12:11:13 +0300

    FEAT: Create Passenger Class to hold passenger name and his bookings

---
+++ PassengerHandler/Passenger.cs
@@ -0,0 +1,27 @@
+﻿using AirportTicketBookingSystem.BookingHandler;
+using AirportTicketBookingSystem.ServiceClasses;
+
+namespace AirportTicketBookingSystem.PassengerHandler
+{
+    public class Passenger
+    {
+        public Name PassengerName { get; set; }
+
+        public List<Booking> PassengerBookings { get; set; }
+
+        public Passenger()
+        {
+            PassengerBookings = new List<Booking>();
+        }
+
+        public void AddBooking(Booking booking)
+        {
+            PassengerBookings.Add(booking);
+        }
+
+        public void RemoveBooking(Booking booking)
+        {
+            PassengerBookings.Remove(booking);
+        }
+    }
+}
