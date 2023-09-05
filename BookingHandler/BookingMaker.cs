156581b96831303f4b150c822621afab3689bc90
Tariq Zaghal <zaghaltariq007@gmail.com>
2023-09-05 14:49:34 +0300

    FEAT: This class organizes the relationship between booking, flight, and passenger by adding booking to passenger and flight

---
+++ BookingHandler/BookingMaker.cs
@@ -0,0 +1,25 @@
+﻿using AirportTicketBookingSystem.FlightHandler;
+using AirportTicketBookingSystem.PassengerHandler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTicketBookingSystem.BookingHandler
+{
+    public class BookingMaker
+    {
+        public static void MakeBooking(Passenger passenger, Booking booking, Flight flight)
+        {
+            passenger.AddBooking(booking);
+            flight.AddBooking(booking);
+        }
+
+        public static void DeleteBooking(Passenger passenger, Booking booking, Flight flight)
+        {
+            passenger.RemoveBooking(booking);
+            flight.RemoveBooking(booking);
+        }
+    }
+}
