7ee4c378625f53c7f69f051dba9504bbbb222922
Tariq Zaghal <zaghaltariq007@gmail.com>
2023-09-05 15:46:53 +0300

    FEAT: Create Enum that maps the manager options to integers

---
+++ ManagerHandler/ManagerOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTicketBookingSystem.ManagerHandler
+{
+    public enum ManagerOptions
+    {
+        AddFlights = 1,
+        InfoAboutDataFormat = 2,
+        LookupFlight = 3,
+        BackToMainMenu = 4
+    }
+}
